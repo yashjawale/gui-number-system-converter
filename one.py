5b5120c1eaba37dc001956a136ad347a00b8ed5e
sahil-s-246 <sahilsrinivas3@gmail.com>
2022-06-12 12:14:02 +0530

    updated one.py

--- one.py
+++ one.py
@@ -1,22 +1,24 @@
-num = input("Enter a number: ")
-frombase = int(input("Enter original base: "))
-tobase = int(input("Enter desired base: "))
+# num = input("Enter a number: ")
+# frombase = int(input("Enter original base: "))
+# tobase = int(input("Enter desired base: "))
 
 # print(num, "From ", frombase, "To", tobase)
+
 
 def convert(num, frombase, tobase):
     decnum = int(num, frombase)
     
-    if(tobase == 2):
-        print(bin(decnum)[2:])
+    if tobase == 2:
+        return bin(decnum)[2:]
     
-    if(tobase == 8):
-        print(bin(decnum)[2:])
+    if tobase == 8:
+        return bin(decnum)[2:]
     
-    if(tobase == 10):
-        print(decnum)
+    if tobase == 10:
+        return decnum
     
-    if(tobase == 16):
-        print(hex(decnum)[2:]) 
+    if tobase == 16:
+        return hex(decnum)[2:]
 
-convert(num, frombase, tobase)
+
+# print(convert(num, frombase, tobase))
